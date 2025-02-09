533d5209ddd72ec781ac3f0eb8d722c726ce12df
轩广哲 <9841161+xuan-guangzhe@user.noreply.gitee.com>
2025-02-10 00:25:54 +0800

    commit 6

--- src/main/java/io/openvidu/basic/java/BasicJavaApplication.java
+++ src/main/java/io/openvidu/basic/java/BasicJavaApplication.java
@@ -2,6 +2,7 @@ package io.openvidu.basic.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("io.openvidu.basic.mapper")	
