f1883493087749dffb44f1314681658ec7f16984
轩广哲 <9841161+xuan-guangzhe@user.noreply.gitee.com>
2025-02-12 23:06:15 +0800

    fix 1

--- src/main/java/io/openvidu/basic/BasicJavaApplication.java
+++ src/main/java/io/openvidu/basic/BasicJavaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("io.openvidu.basic.mapper")	
-@ComponentScan("io.openvidu.basic.controller")
+
 public class BasicJavaApplication {
 
 	public static void main(String[] args) {
