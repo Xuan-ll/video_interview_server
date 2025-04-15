afce25b769fdc901cef184c0b0f2a15c6ce68345
轩广哲 <9841161+xuan-guangzhe@user.noreply.gitee.com>
2025-04-15 13:24:44 +0800

    add ssl

--- target/classes/db/init.sql
+++ target/classes/db/init.sql
@@ -8,6 +8,13 @@ CREATE TABLE IF NOT EXISTS interview (
     room_name VARCHAR(255) NOT NULL,
     scheduled_time DATETIME NOT NULL,
     participants TEXT NOT NULL,
+    interviewers TEXT NOT NULL,
+    hr_name    VARCHAR(255) NOT NULL,
+    interview_status     INT NOT NULL DEFAULT 0,
+    position   VARCHAR(255) NOT NULL,
+    interview_period     VARCHAR(255) NOT NULL, 
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
+    updated_at DATETIME NOT NULL,
+    interview_password   VARCHAR(255) NOT NULL,
+    interview_text TEXT 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
